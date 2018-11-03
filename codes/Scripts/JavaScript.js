515882c683db63412494736acab5c6149af5c522
ojeshwini H P <ojeshwini96@gmail.com>
2018-11-04 01:59:29 +0530

    first commit

---
+++ codes/Scripts/JavaScript.js
@@ -0,0 +1,27 @@
+﻿
+    function getStateList()
+    {  
+        //debugger;
+    var countryId = $("#countryDropDown").val();
+        $.ajax({
+            url: '/AddGroupCompany/GetStateList',
+            type: 'POST',
+            datatype: 'application/json',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                stateId: +stateId
+            }),
+success: function(result)
+            {
+        $("#stateDropDown").html("");
+    $.each($.parseJSON(result), function (i, state) {
+        $("#stateDropDown").append($('<option></option>').val(state.StateID).html(state.StateName));
+    });
+
+},
+error: function()
+            {
+        alert("Whooaaa! Something went wrong..")
+    },
+});
+    } 
